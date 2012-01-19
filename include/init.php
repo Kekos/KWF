e24abca09cf266f3bafb76a017e57c17a659d35a
Christoffer Lindahl <christoffer@kekos.se>
2012-01-19 23:16:00 +0100

    Bumped version to 3.7

--- include/init.php
+++ include/init.php
@@ -3,14 +3,14 @@
  * KWF System: Initiation
  * 
  * @author Christoffer Lindahl <christoffer@kekos.se>
- * @date 2011-11-15
+ * @date 2012-01-19
  * @version 3.0
  */
 
 if (!defined('BASE'))
   die('Access denied.');
 
-define('KWF_VERSION', '3.6');
+define('KWF_VERSION', '3.7');
 
 require(BASE . 'include/errors.php');
 require(BASE . 'include/functions.php');
