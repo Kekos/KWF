85c28eeeb8d691b10ad168ac5f67314d439e3de0
Christoffer Lindahl <christoffer@kekos.se>
2012-08-21 20:51:10 +0200

    Removed magic_quotes_gpc setting since it can not be set in runtime anyway.

--- include/init.php
+++ include/init.php
@@ -3,14 +3,14 @@
  * KWF System: Initiation
  * 
  * @author Christoffer Lindahl <christoffer@kekos.se>
- * @date 2012-01-19
- * @version 3.0
+ * @date 2012-08-21
+ * @version 4.0
  */
 
 if (!defined('BASE'))
   die('Access denied.');
 
-define('KWF_VERSION', '3.7');
+define('KWF_VERSION', '4.0');
 
 require(BASE . 'include/errors.php');
 require(BASE . 'include/functions.php');
@@ -18,7 +18,6 @@ require(BASE . 'include/functions_extra.php');
 
 ini_set('register_globals', false);
 ini_set('register_long_arrays', false);
-ini_set('magic_quotes_gpc', false);
 ini_set('magic_quotes_runtime', false);
 date_default_timezone_set('Europe/Paris');
 ?>
