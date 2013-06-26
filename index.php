c3c21c8400f0f4f579076e5d475dfddc04e34bad
Christoffer Lindahl <christoffer@kekos.se>
2013-06-26 21:30:38 +0200

    Updated the index.php file so it uses the new Request, Router and Page classes.

--- index.php
+++ index.php
@@ -5,15 +5,15 @@ require(BASE . 'include/init.php');
 
 $route = (isset($_GET['r']) ? $_GET['r'] : '');
 
-$request = new Request(new Session(), new Cookie());
-$page_model = new PageModel();
+$request = new Request(new Session(), new Cookie(), $route);
+$page_model = new FilePageModel();
 
 Language::configure($request, LANGUAGE_SESSION, LANGUAGE_DEFAULT);
 Language::acceptHeader();
 
-$router = new Router($route, $request, $page_model);
+$router = new Router($request, $page_model);
 $page = $router->getPage();
-$page->runControllers();
+$page->runControllers($request);
 
 echo $page->getResponseContent();
 
